093f985bf5562f3060f6a068f237f7871a733816
Valsk0 <contact@valentin-dumont.com>
2024-10-02 09:40:40 +0200

    ajout de workflows

---
+++ test.py
@@ -0,0 +1,12 @@
+import myfunct
+
+def test_predict_tags():
+    sentence = "How to train a model in TensorFlow?"
+    n = 5
+    assert isinstance(myfunct.predict_tags(sentence, n), list), "La fonction doit retourner une liste."
+
+def test_predict_tags_output():
+    sentence = "How to compile a C program using gcc on Ubuntu ?"
+    n = 5
+    assert myfunct.predict_tags(sentence, n) == ['c', 'linux', 'gcc', 'ubuntu', 'compilation'], "La fonction ne retourne pas les tags attendus."
+    
